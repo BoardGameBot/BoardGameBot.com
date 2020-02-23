811f8318ca9545d2aad39ad0a6477055792ad657
flamecoals <flamecoals@users.noreply.github.com>
2020-02-22 23:42:40 -0800

    Discord.js hello world

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-console.log('Hello world!');
+import * as Discord from 'discord.js';
+
+const client = new Discord.Client();
+const token = process.env.DISCORD_TOKEN;
+if (!token) {
+  throw new Error('No token found!!');
+} else {
+  console.log('Using token: '+ token);
+}
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login(token);
