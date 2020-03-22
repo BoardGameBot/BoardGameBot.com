d1ed2e6a5398f405c680eee545e3c12974830b3a
flamecoals <flamecoals@users.noreply.github.com>
2020-03-21 18:46:32 -0700

    Formatting master

--- src/games/tictactoe/handlers/MoveHandler.ts
+++ src/games/tictactoe/handlers/MoveHandler.ts
@@ -3,31 +3,31 @@ import { isCommand } from '../../../util';
 import { save } from '../../../save';
 
 export default class MoveHandler extends GameHandler {
-    async handlesMessage() {
-        return isCommand(this.channel, this.msg, 'move');
-    }
+  async handlesMessage() {
+    return isCommand(this.channel, this.msg, 'move');
+  }
 
-    async reply() {
-        const splitMsg = this.msg.content.split(' ');
-        if (!this.isValidCommand(splitMsg)) {
-            return this.simpleReply('Invalid command. Usage: .move <<CELL NUMBER>>');
-        }
-        if (!this.isCurrentPlayer()) {
-            return this.simpleReply('It is not your turn!');
-        }
-        const index = parseInt(splitMsg[1]);
-        const state = this.game.getState();
-        const cells = state.G.cells;
-        if (index < 0 || index >= cells.length || cells[index] !== null) {
-            return this.simpleReply('Invalid cell!');
-        }
-        this.game.moves.clickCell(index);
-
-        await this.save();
-        return this.simpleReply(JSON.stringify(this.game.getState().G.cells));
+  async reply() {
+    const splitMsg = this.msg.content.split(' ');
+    if (!this.isValidCommand(splitMsg)) {
+      return this.simpleReply('Invalid command. Usage: .move <<CELL NUMBER>>');
     }
-
-    isValidCommand(splitMsg: string[]) {
-        return splitMsg.length === 2 && parseInt(splitMsg[1]) >= 0;
+    if (!this.isCurrentPlayer()) {
+      return this.simpleReply('It is not your turn!');
     }
+    const index = parseInt(splitMsg[1]);
+    const state = this.game.getState();
+    const cells = state.G.cells;
+    if (index < 0 || index >= cells.length || cells[index] !== null) {
+      return this.simpleReply('Invalid cell!');
+    }
+    this.game.moves.clickCell(index);
+
+    await this.save();
+    return this.simpleReply(JSON.stringify(this.game.getState().G.cells));
+  }
+
+  isValidCommand(splitMsg: string[]) {
+    return splitMsg.length === 2 && parseInt(splitMsg[1]) >= 0;
+  }
 }
--- src/handlers/InviteHandler.ts
+++ src/handlers/InviteHandler.ts
@@ -7,79 +7,79 @@ import { ChannelType, Reply, User } from '../messaging';
 type MaybeReply = Reply | undefined;
 
 export default class InviteHandler extends MessageHandler {
-    name = 'Invite';
+  name = 'Invite';
 
-    async handlesMessage() {
-        return this.msg.channel.type === ChannelType.PUBLIC_GROUP && isCommand(this.channel, this.msg, 'invite');
-    }
+  async handlesMessage() {
+    return this.msg.channel.type === ChannelType.PUBLIC_GROUP && isCommand(this.channel, this.msg, 'invite');
+  }
 
-    async reply(): Promise<Reply> {
-        const args = this.msg.content.split(' ');
-        const gameCode = args[1];
-        const users: User[] = (this.msg.mentions || []).map(mention => mention.user);
-        users.unshift(this.msg.author);
-        // TODO: check min/max number of users for specific game
-        const checks: MaybeReply[] = [
-            this.checkGameInProgress(),
-            this.checkNumberOfArguments(args),
-            this.checkValidGame(gameCode),
-            this.checkValidUserSelection(users),
-            this.checkMinNumberOfUsers(users),
-        ];
-        const errorReply = checks.find(maybeReply => maybeReply !== undefined);
-        if (errorReply) {
-            return errorReply;
-        }
-        return this.invite(users, gameCode);
+  async reply(): Promise<Reply> {
+    const args = this.msg.content.split(' ');
+    const gameCode = args[1];
+    const users: User[] = (this.msg.mentions || []).map(mention => mention.user);
+    users.unshift(this.msg.author);
+    // TODO: check min/max number of users for specific game
+    const checks: MaybeReply[] = [
+      this.checkGameInProgress(),
+      this.checkNumberOfArguments(args),
+      this.checkValidGame(gameCode),
+      this.checkValidUserSelection(users),
+      this.checkMinNumberOfUsers(users),
+    ];
+    const errorReply = checks.find(maybeReply => maybeReply !== undefined);
+    if (errorReply) {
+      return errorReply;
     }
+    return this.invite(users, gameCode);
+  }
 
-    private checkGameInProgress(): MaybeReply {
-        if (this.channel.currentGame) {
-            return this.simpleReply('There is a game in progress. Wait for it to end in order to start a new one.');
-        }
+  private checkGameInProgress(): MaybeReply {
+    if (this.channel.currentGame) {
+      return this.simpleReply('There is a game in progress. Wait for it to end in order to start a new one.');
     }
+  }
 
-    private checkNumberOfArguments(args: String[]): MaybeReply {
-        if (args.length < 3) {
-            return this.simpleReply('Not enough arguments. Correct usage: ".invite GAME @PLAYER1 @PLAYER2..."');
-        }
+  private checkNumberOfArguments(args: String[]): MaybeReply {
+    if (args.length < 3) {
+      return this.simpleReply('Not enough arguments. Correct usage: ".invite GAME @PLAYER1 @PLAYER2..."');
     }
+  }
 
-    private checkValidGame(gameCode: string): MaybeReply {
-        if (!(gameCode in GAMES_MAP)) {
-            return this.simpleReply('Invalid game. Check https://boardgamebot.com/games to see list of games.');
-        }
+  private checkValidGame(gameCode: string): MaybeReply {
+    if (!(gameCode in GAMES_MAP)) {
+      return this.simpleReply('Invalid game. Check https://boardgamebot.com/games to see list of games.');
     }
+  }
 
-    private checkValidUserSelection(users: User[]): MaybeReply {
-        const ids = users.map(user => user.id.value);
-        const idsSet = new Set(ids);
-        if (ids.length != idsSet.size) {
-            return this.simpleReply('Invalid selection of users.');
-        }
+  private checkValidUserSelection(users: User[]): MaybeReply {
+    const ids = users.map(user => user.id.value);
+    const idsSet = new Set(ids);
+    if (ids.length != idsSet.size) {
+      return this.simpleReply('Invalid selection of users.');
     }
+  }
 
-    private checkMinNumberOfUsers(users: User[]) {
-        if (users.length <= 1) {
-            return this.simpleReply('Not enough users to play this game!');
-        }
+  private checkMinNumberOfUsers(users: User[]) {
+    if (users.length <= 1) {
+      return this.simpleReply('Not enough users to play this game!');
     }
+  }
 
-    private async invite(users: User[], gameCode: string): Promise<Reply> {
-        const players = users.map(user => convertUserToPlayer(user));
-        const creator = this.msg.author;
-        this.channel.invites = {
-            players,
-            accepted: [convertUserToPlayer(creator)],
-            gameCode,
-        };
-        await save(this.state);
-        const invitees = players
-            .filter(player => player.id !== creator.id)
-            .map(invitee => invitee.username)
-            .join(', ');
-        return this.simpleReply(
-            `${invitees}, do you want to play ${gameCode} with <@${creator.id.value}> ? Use ".accept" to accept, or ".reject" to reject invite.`,
-        );
-    }
+  private async invite(users: User[], gameCode: string): Promise<Reply> {
+    const players = users.map(user => convertUserToPlayer(user));
+    const creator = this.msg.author;
+    this.channel.invites = {
+      players,
+      accepted: [convertUserToPlayer(creator)],
+      gameCode,
+    };
+    await save(this.state);
+    const invitees = players
+      .filter(player => player.id !== creator.id)
+      .map(invitee => invitee.username)
+      .join(', ');
+    return this.simpleReply(
+      `${invitees}, do you want to play ${gameCode} with <@${creator.id.value}> ? Use ".accept" to accept, or ".reject" to reject invite.`,
+    );
+  }
 }
