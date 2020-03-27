789ec9f1bc0bc58f5f075ab4c30ff7f066dac0c0
flamecoals <flamecoals@users.noreply.github.com>
2020-03-27 00:48:19 -0700

    Populating buckets...

---
+++ src/games/mosaic/constants.ts
@@ -0,0 +1,34 @@
+import { Color, Bucket, Board, BoardTemplate } from './definitions';
+
+export const DEFAULT_TEMPLATE: BoardTemplate = {
+  template: [
+    [Color.BLUE, Color.YELLOW, Color.RED, Color.BLACK, Color.GREEN],
+    [Color.GREEN, Color.BLUE, Color.YELLOW, Color.RED, Color.BLACK],
+    [Color.BLACK, Color.GREEN, Color.BLUE, Color.YELLOW, Color.RED],
+    [Color.RED, Color.BLACK, Color.GREEN, Color.BLUE, Color.YELLOW],
+    [Color.YELLOW, Color.RED, Color.BLACK, Color.GREEN, Color.BLUE],
+  ],
+};
+
+export const DEFAULT_BOARD: Board = {
+  rows: [{ maxSize: 1 }, { maxSize: 2 }, { maxSize: 3 }, { maxSize: 4 }, { maxSize: 5 }],
+  board: [
+    [Color.NONE, Color.NONE, Color.NONE, Color.NONE, Color.NONE],
+    [Color.NONE, Color.NONE, Color.NONE, Color.NONE, Color.NONE],
+    [Color.NONE, Color.NONE, Color.NONE, Color.NONE, Color.NONE],
+    [Color.NONE, Color.NONE, Color.NONE, Color.NONE, Color.NONE],
+    [Color.NONE, Color.NONE, Color.NONE, Color.NONE, Color.NONE],
+  ],
+  penaltyRow: [Color.NONE, Color.NONE, Color.NONE, Color.NONE, Color.NONE, Color.NONE, Color.NONE],
+  points: 0,
+};
+
+export const DEFAULT_BAG: Bucket = {
+  red: 20,
+  yellow: 20,
+  black: 20,
+  blue: 20,
+  green: 20,
+};
+
+export const COLOR_ARRAY = [Color.RED, Color.YELLOW, Color.BLACK, Color.BLUE, Color.GREEN];
---
+++ src/games/mosaic/definitions.ts
@@ -0,0 +1,60 @@
+export enum Color {
+  NONE = 'none',
+  RED = 'red',
+  YELLOW = 'yellow',
+  BLACK = 'black',
+  BLUE = 'blue',
+  GREEN = 'green',
+}
+
+export interface BoardTemplate {
+  template: Color[][]; // 5x5 board template
+}
+
+export interface Board {
+  rows: Bucket[];
+  board: Color[][]; // 5x5 board template
+  penaltyRow: Color[];
+  points: number;
+}
+
+export interface Bucket {
+  maxSize?: number;
+  red?: number;
+  yellow?: number;
+  black?: number;
+  blue?: number;
+  green?: number;
+}
+
+export interface MosaicGameState {
+  boards: Board[];
+  boardTemplate: BoardTemplate;
+  bag: Bucket;
+  // Players | # Buckets
+  // --------|----------
+  // 2       | 5
+  // 3       | 7
+  // 4       | 9
+  restrictedBuckets: Bucket[];
+  centerBucket: Bucket;
+  secondaryBag: Bucket;
+}
+
+export enum BucketType {
+  CENTER,
+  RESTRICTED,
+}
+
+export enum RowType {
+  NORMAL,
+  PENALTY,
+}
+
+export interface MoveDetails {
+  bucketType: BucketType;
+  bucketIndex?: number;
+  color: Color;
+  rowType: RowType;
+  rowIndex?: number;
+}
